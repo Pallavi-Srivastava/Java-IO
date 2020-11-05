41338f40a417556c561f17a366a05a2dcb6bfc78
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-06 05:18:57 +0530

    Added:Code For Read Employee Payroll File

---
+++ src/test/java/com/blz/io/EmployeePayrollServiceTest.java
@@ -0,0 +1,28 @@
+package com.blz.io;
+
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class EmployeePayrollServiceTest {
+
+	@Test
+	public void given2EmloyeesDetailsWrittenToFileShouldMatchWithEntries() {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(908, "Pallavi", 50000.0),
+				new EmployeePayrollData(909, "Anni", 70000.0) };
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(com.blz.io.EmployeePayrollService.IOService.FILE_IO);
+		employeePayrollService.printData(com.blz.io.EmployeePayrollService.IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(com.blz.io.EmployeePayrollService.IOService.FILE_IO);
+		Assert.assertEquals(2, entries);
+	}
+
+	@Test
+	public void readTheEmployeePayrollFile() {
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollService.readEmployeePayrollData(com.blz.io.EmployeePayrollService.IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(com.blz.io.EmployeePayrollService.IOService.FILE_IO);
+		Assert.assertEquals(2, entries);
+	}
+}
